7e0b335019ee942363109d4ed7042a42030b9199
Liam Fruzyna <liamfruzyna@gmail.com>
2021-01-30 22:48:09 -0600

    Added pass aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,9 @@ alias la="ls -A"
 alias ll="ls -lA"
 alias lr="ls -rA"
 
+alias show="pass show"
+alias copy="pass show -c"
+
 # machines
 set -x elmago "liam@192.168.1.35"
 set -x rossy "mail929@192.168.1.31"
