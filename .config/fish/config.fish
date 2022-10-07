5619c6f80237e6bfa9dbd72cf55122dc96261b90
Liam Fruzyna <liam@fruzyna.net>
2022-10-07 09:33:06 -0500

    Updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,15 @@
-# custom greeting
-function fish_greeting
-	date
-	echo
-end
-funcsave fish_greeting
-
 # aliases
 alias la="ls -A"
 alias ll="ls -lA"
 alias lr="ls -rA"
 
-alias show="pass show"
-alias copy="pass show -c"
+alias pshow="pass show"
+alias pcopy="pass show -c"
 
 # machines
-set -x elmago "liam@192.168.1.35"
-set -x rossy "mail929@192.168.1.31"
-set -x benny "mail929@192.168.1.32"
-set -x vpn "pi@192.168.1.36"
-set -x astrobin "root@192.168.1.37"
-set -x rajai "root@192.168.1.38"
+set -x rossy "liam@192.168.1.11"
+set -x elmago "liam@192.168.16.12"
+set -x cone "liam@192.168.16.14"
+set -x benny "liam@192.168.7.2"
+set -x astrobin "root@192.168.16.37"
+set -x rajai "root@192.168.16.38"
