bf131be0ad742dd01717fff526915e9b1ffe94dd
Liam Fruzyna <liam@fruzyna.net>
2025-05-19 15:36:22 -0500

    Updated fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,3 @@
 # aliases
-alias la="ls -A"
 alias ll="ls -lA"
-alias lr="ls -rA"
-
-alias pshow="pass show"
-alias pcopy="pass show -c"
-
-# machines
-set -x rossy "liam@192.168.1.11"
-set -x elmago "liam@192.168.16.12"
-set -x cone "liam@192.168.16.14"
-set -x benny "liam@192.168.7.2"
-set -x astrobin "root@192.168.16.37"
-set -x rajai "root@192.168.16.38"
+alias grep="grep -In --color"
