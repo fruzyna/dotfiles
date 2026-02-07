c8f3e8e284b1a2600806bcc7d254549fc88dc00a
Liam Fruzyna <liam@fruzyna.net>
2026-02-06 19:20:54 -0600

    Dotfiles from work

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
 # aliases
 alias ll="ls -lA"
+alias cat="cat -n"
 alias grep="grep -In --color"
+alias igrep="grep -Ini --color"
+
+alias log="git log --pretty=format:'%C(yellow)%h %C(blue)%ad %C(green)%an%Creset%x09%s%(decorate:pointer= ,separator= ,prefix=%n,suffix)' --date=short -10"
+alias summary="git config --get remote.origin.url; echo "----------"; git status -s | command grep -v ??; echo "----------"; git log --pretty=format:'%C(yellow)%h %C(blue)%ad %C(green)%an%Creset%x09%s%(decorate:pointer= ,separator= ,prefix=%n,suffix)' --date=short -10"
--- .vimrc
+++ .vimrc
@@ -1,10 +1,10 @@
-"python3 from powerline.vim import setup as powerline_setup
-"python3 powerline_setup()
-"python3 del powerline_setup
-set laststatus=2 " Always display the statusline in all windows
-set showtabline=2 " Always display the tabline, even if there is only one tab
-set noshowmode " Hide the default mode text (e.g. -- INSERT -- below the statusline)
-set t_Co=256
-set backspace=indent,eol,start
+syntax on
+set nowrap
+set number
+set cursorline
+set hlsearch
+
+set expandtab
 set tabstop=4
 set shiftwidth=4
+set autoindent
